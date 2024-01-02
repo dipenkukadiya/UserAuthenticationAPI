726a49fef103e9ec7d5356bca909a9d8c4dab382
dipen kukadiya <dipenkukadiya@gmail.com>
2024-01-02 18:28:15 +0530

    authntication security add

--- src/main/java/com/test/UserAuth/config/SecurityConfig.java
+++ src/main/java/com/test/UserAuth/config/SecurityConfig.java
@@ -1,32 +1,26 @@
+
 package com.test.UserAuth.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurationAdapter {
-
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-            .authorizeRequests()
-                .antMatchers("/users/**").authenticated() // Require authentication for "/users/" endpoint
-                .and()
-            .formLogin()
-                .permitAll() // Allow access to the login page
-                .and()
-            .logout()
-                .permitAll(); // Allow access to the logout endpoint
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        PasswordEncoder passwordEncoder = passwordEncoder();
+        auth.inMemoryAuthentication()
+                .passwordEncoder(passwordEncoder)
+                .withUser("dipen")
+                .password(passwordEncoder.encode("Dipen"))
+                .roles("USER");    }
+                 @Bean
+        public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 }
